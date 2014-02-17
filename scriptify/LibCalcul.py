32a6b117d4cdf1d9e699bcbfc7dc477ab6a6d869
unknown <FX09079@LADWE014672.adgroup.michelin.com>
2014-02-17 14:56:54 +0100

    Release

--- scriptify/LibCalcul.py
+++ scriptify/LibCalcul.py
@@ -1,18 +1,19 @@
 from Nombre import Nombre
 from Chiffre import Chiffre
 
-class LibCalcul:    
+class LibCalcul: 
     @staticmethod
     def getNombre(nombre):
-#         print "getNombre avec", nombre
         return Nombre(nombre)
     
     @staticmethod
     def getChiffre(chiffre):
-#         print "getChiffre avec", chiffre
         return Chiffre(chiffre)
     
     @staticmethod
     def additionne(nombre, chiffre):
-#         print "additionne", nombre, chiffre
         return Nombre(nombre.toInt() + chiffre.toInt())
+    
+    @staticmethod
+    def printNombre(nombre):
+        print nombre.toInt()
